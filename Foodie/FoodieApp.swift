2d300434d64166a35a830ccc0f563b102f750de4
Christian Manzaraz <magnusmanz@icloud.com>
2025-03-05 12:59:30 -0300

    Initial Commit

---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by Christian Manzaraz on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
